8935cd739c27091674618560c31621f1ad1fffc9
Justin Carpenter <Justin.Carpenter@iam.local>
2017-12-04 16:56:24 -0600

    Added looking at objects
    Add the generic object script to name things that are looked at

--- Assets/Scripts/IActivatable.cs
+++ Assets/Scripts/IActivatable.cs
@@ -1,9 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IActivatable  {
-
-    void DoActivate();
-
+﻿public interface IActivatable 
+{
+   string NameText { get; }
+   void DoActivate();
 }
